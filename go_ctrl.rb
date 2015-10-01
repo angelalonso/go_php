d3b4d8e70aa689697f92a3dd0ef407ad7eed2092
root <root@debian.debianvm>
2015-10-01 20:24:22 +0200

    board works

---
+++ go_ctrl.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+
+if (!ARGV[0].nil?) 
+  puts ("You selected " + ARGV[0])
+else 
+  puts "meh"
+end
--- index.php
+++ index.php
@@ -1,26 +1,77 @@
-HTML>
+<!DOCTYPE html>
  <HEAD>
-  <title>PHP Test</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>GO Game</title>
+  <meta name="description" content="">
+
+  <link rel="stylesheet" href="css/main.css">
  </HEAD>
  <BODY>
 
  <?php
+$servername = "localhost";
+## YES, I KNOW! (this code is being tested in an isolated VM nevertheless)
+$username = "root";
+$password = "root 123";
+$dbname = "goDB";
 
 $colNumber = 19;
 $rowNumber = 19;
 
-echo '<table>';
-for ( $rowCounter = 0; $rowCounter < $rowNumber; $rowCounter += 1) {
-  echo '<tr>';
-  for ( $colCounter = 0; $colCounter < $colNumber; $colCounter += 1) {
-    echo '<td>' , $colCounter , '</td>';
-  }
-  echo '</tr>';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if (isset($_GET['clicked'])) {
+    $position = $_GET['clicked'];
+    $command = "/usr/bin/ruby /var/www/html/go_php/go_ctrl.rb ". $position;
+    echo exec($command);
+}else{
+    // Fallback behaviour goes here
 }
 
-echo '</table>';
+echo '<TABLE id="gametable">';
+
+$sql = "SELECT col_row, stone FROM stonetable_empty";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($entry = $result->fetch_assoc()) {
+        $col = (string)str_split($entry["col_row"],2)[0];
+        $row = (string)str_split($entry["col_row"],2)[1];
+        if ( $row == "00" ) {echo '<TR>';}
+        if ( $row == strval($colNumber) ) {echo '</TR>';}
+
+        if ($entry["stone"] == "black") {
+          echo '<TD id="stone_black" ></TD>';
+        } elseif ($entry["stone"] == "white") {
+          echo '<TD id="stone_white" ></TD>';
+        } else {
+          echo '<TD id="stone_empty" ><a href="?clicked=' , $entry["col_row"] , '"></a></TD>';
+        }
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+echo '</TABLE>';
+
+$result = foo(40,50);
+echo $result;
+
+function foo($arg_1, $arg_2)
+{
+    echo "Example function.\n";
+    return $arg_2;
+}
+
 
  ?>
 
  </BODY>
-</HTML>
